1740c51955777b1be7119ec7b26e286082084bf2
hllinc <hllinc@163.com>
2018-04-25 17:38:18 +0800

    用户管理初步。

--- src/app/sys/user/user.component.ts
+++ src/app/sys/user/user.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {User} from '../models/user';
 import {UserService} from './user.service';
+import {Page} from '../../models/page';
 
 @Component({
   selector: 'app-user',
@@ -8,15 +9,44 @@ import {UserService} from './user.service';
   styleUrls: ['./user.component.scss']
 })
 export class UserComponent implements OnInit {
-  public users: User[];
+  public users: User[] = [];
   public tableLoading = true;
+
+  page: Page = new Page(1, 10);
+  _allChecked = false;
+  _indeterminate = false;
+  _displayData = [];
   constructor(private userService: UserService) { }
 
   ngOnInit() {
-    this.userService.getUsers().subscribe(data => {
-      this.users = data;
+    this.userService.getUsersByPage(this.page).subscribe(data => {
+      this.users = data.content;
       this.tableLoading = false;
     });
+  }
+  _displayDataChange($event) {
+    this._displayData = $event;
+    this._refreshStatus();
+  }
+
+  _refreshStatus() {
+    const allChecked = this._displayData.every(value => value.checked === true);
+    const allUnChecked = this._displayData.every(value => !value.checked);
+    this._allChecked = allChecked;
+    this._indeterminate = (!allChecked) && (!allUnChecked);
+  }
+
+  _checkAll(value) {
+    if (value) {
+      this._displayData.forEach(data => {
+        data.checked = true;
+      });
+    } else {
+      this._displayData.forEach(data => {
+        data.checked = false;
+      });
+    }
+    this._refreshStatus();
   }
 
 }
--- src/app/sys/user/user.service.ts
+++ src/app/sys/user/user.service.ts
@@ -1,6 +1,7 @@
 import {Injectable} from '@angular/core';
 import {DataService} from '../../services/data.service';
 import {Observable} from 'rxjs/Observable';
+import {Page} from '../../models/page';
 
 @Injectable()
 export class UserService {
@@ -9,11 +10,20 @@ export class UserService {
   }
 
   /**
-   * 获取用户
+   * 获取本地mock数据用户
    * @returns {Observable<any>}
    */
   getUsers(): Observable<any> {
     return this.dataService.getData('./mock/user.json', true);
+  }
+
+  /**
+   * 分页查询用户
+   * @param {Page} page
+   * @returns {Observable<any>}
+   */
+  getUsersByPage(page: Page): Observable<any> {
+    return this.dataService.getData('/sys/user/findPage?number=' + page.number + '&size=' + page.size);
   }
 
 }
