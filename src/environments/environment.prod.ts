3e7f3bdb2ead0a462061fe7f793454b187c75615
hllinc <hllinc@163.com>
2019-01-09 23:36:15 +0800

    修改编译命令。

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,10 @@
 export const environment = {
-  production: true
+  production: true,
+  serverHost: 'http://localhost:8089', // 服务器路径
+  hmr: false, // 是否开启模块热更新（针对开发）
+  title: 'Angular Start', // 系统名称（请根据实际项目进行修改）
+  domain: '', // 请求的域名（请根据实际项目进行修改）
+  mockDomain: '', // 模拟请求的域名（请根据实际项目进行修改）
+  tokenName: '', // 令牌名称（请根据实际项目进行修改）
+  appDownloadUrl: '' // app下载路径
 };
